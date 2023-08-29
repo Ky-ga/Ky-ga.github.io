c00e5daa6538cb9f863046442f05ba5f5a2f327c
Ky-ga <elev.fabian.glimbrand@skola.lund.se>
2023-08-29 21:28:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<link rel="shortcut icon" href="images/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cultist</title>
+    <link rel="stylesheet" href="Style/style.css">
+</head>
+<body>
+    <h1 class="header">Cultist</h1>
+    <main>
+        <p class="text-center">In the heart of the digital realm, a mysterious figure emerged, known only as Dominus...</p>
+        <form action="process.php" method="post">
+            <button id="runPhpButton" class="button" type="submit"><b>JOIN THE CULT?</b></button>
+        </form>
+        <div id="output" class="text-center">
+            <!-- PHP output will be displayed here -->
+        </div>
+    </main>
+    <script src="script.js"></script>
+</body>
+</html>
