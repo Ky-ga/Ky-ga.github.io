6afdf7118a3221ee318cc02425804d67b9aeedb2
Ky-ga <elev.fabian.glimbrand@skola.lund.se>
2023-08-29 11:24:29 +0200

    Update process.php

--- process.php
+++ process.php
@@ -1,12 +1,8 @@
 <?php
-// Process and display the IP address
-$ip = get_ip_address();
-echo "Your IP is: " . $ip;
+// Process your PHP code here
+$ip = $_SERVER["REMOTE_ADDR"];
+$processedOutput = "Your IP is: " . $ip;
 
-function get_ip_address() {
-  // ... your existing IP retrieval logic ...
-
-  // return unreliable ip since all else failed
-  return $_SERVER['REMOTE_ADDR'];
-}
+// Send the processed output
+echo $processedOutput;
 ?>
